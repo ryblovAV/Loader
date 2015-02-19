70d6f873fded1e990c8e31dc494e0d4d9368df54
Alexander Ryblov <ryblov.a.v@gmail.com>
2015-02-19 16:21:18 +0300

    change reference to Utils

--- src/main/scala/org/loader/models/Characteristic.scala
+++ src/main/scala/org/loader/models/Characteristic.scala
@@ -2,12 +2,12 @@ package org.loader.models
 
 import java.util.Date
 
-import org.loader.builders.Utills
+import org.loader.builders.Utils
 
 case class Characteristic(charTypeCd: String,
                 charVal: String = " ",
                 adhocCharVal: String = " ",
-                effDt: Date  = Utills.getDefaultDt) {
+                effDt: Date  = Utils.getDefaultDt) {
 
   def isEmpty = ((charVal == null) || (charVal == " ")) && ((adhocCharVal == null) || (adhocCharVal == " "))
 
