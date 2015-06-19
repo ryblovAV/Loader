4050e858d9aed5d5af5717a909cf4a93688b7cbe
Alexander Ryblov <ryblov.a.v@gmail.com>
2015-06-19 16:17:12 +0300

    add builder for Service Agrement

---
+++ src/main/scala/org/loader/builders/general/SaBuilder.scala
@@ -0,0 +1,24 @@
+package org.loader.builders.general
+
+import org.loader.models.Characteristic
+import org.loader.pojo.sa.{SaCharEntity, SaEntity}
+
+object SaBuilder {
+
+  implicit def buildChar(char: Characteristic): SaCharEntity = {
+
+    val saChar = new SaCharEntity()
+    saChar.charTypeCd = char.charTypeCd
+    saChar.effdt = char.effDt
+    saChar.charVal = char.charVal
+    saChar.adhocCharVal = char.adhocCharVal
+
+    saChar
+  }
+
+  def addChar(sa: SaEntity,char: Characteristic) = {
+    if (!char.isEmpty)
+      sa.saCharEntitySet.add(char)
+  }
+
+}
