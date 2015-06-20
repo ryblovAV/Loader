9b34e8baf0ad6f9396deece27514107dbc66d96f
Alexander Ryblov <ryblov.a.v@gmail.com>
2015-06-20 17:44:34 +0300

    add test for translate tension

--- src/test/scala/org/loader/db/BuilderTest.scala
+++ src/test/scala/org/loader/db/BuilderTest.scala
@@ -1,7 +1,7 @@
 package org.loader.db
 
 import grizzled.slf4j.Logging
-import org.loader.builders.gesk.{OkonhDictionary, AddressBuilderG}
+import org.loader.builders.gesk.{SaBuilderG, OkonhDictionary, AddressBuilderG}
 import org.loader.out.gesk.objects.Address
 import org.scalatest.FunSuite
 
@@ -26,6 +26,13 @@ class BuilderTest extends FunSuite with Logging {
     assert(OkonhDictionary.getOkonh(Some("86")) === Some("999108"))
     assert(OkonhDictionary.getOkonh(None) === None)
     assert(OkonhDictionary.dic.foldLeft(0)((c,t) => c + 1) === 107)
+  }
+
+  test("translate napryag") {
+    assert(SaBuilderG.translateNaprayg(Some("нн"),None) == Some("0.4"))
+    assert(SaBuilderG.translateNaprayg(Some("zzz"),Some("210")) == Some("110"))
+    assert(SaBuilderG.translateNaprayg(Some("сн"),Some("210")) == Some("110"))
+    assert(SaBuilderG.translateNaprayg(Some("сн"),Some("0")) == Some("6"))
   }
 
 }
