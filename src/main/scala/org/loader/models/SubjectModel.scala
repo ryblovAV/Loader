0cf9e5e5e610edafac2e07faa170136d71537a22
Alexander Ryblov <ryblov.a.v@gmail.com>
2015-06-26 15:16:25 +0300

    change id_plat to plat

--- src/main/scala/org/loader/models/SubjectModel.scala
+++ src/main/scala/org/loader/models/SubjectModel.scala
@@ -1,6 +1,7 @@
 package org.loader.models
 
-import org.loader.out.gesk.objects.Potr
+import org.loader.out.gesk.objects.{Plat, Potr}
+import org.loader.pojo.acct.AcctEntity
 import org.loader.pojo.mr.MrEntity
 import org.loader.pojo.per.PerEntity
 import org.loader.pojo.sa.SaEntity
@@ -8,5 +9,5 @@ import org.loader.pojo.sp.SpEntity
 
 case class ObjectModel(potr: Potr, sp: SpEntity, sa:SaEntity, mr:MrEntity)
 
-case class SubjectModel(idPlat: String, per: PerEntity, objects: List[ObjectModel])
+case class SubjectModel(plat: Plat, per: PerEntity, acct: AcctEntity, objects: List[ObjectModel])
 
