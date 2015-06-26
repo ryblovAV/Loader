2f9e19e7a995d3dc4839751e858afa7d3dfbebe4
Alexander Ryblov <ryblov.a.v@gmail.com>
2015-06-26 15:16:00 +0300

    add start meter read for service agreement

--- src/main/scala/org/loader/builders/gesk/SaSpBuilderG.scala
+++ src/main/scala/org/loader/builders/gesk/SaSpBuilderG.scala
@@ -1,18 +1,25 @@
 package org.loader.builders.gesk
 
 import org.loader.builders.general.{DateBuilder, KeysBuilder}
+import org.loader.pojo.mr.MrEntity
 import org.loader.pojo.sa.SaEntity
 import org.loader.pojo.sasp.SaSpEntity
 import org.loader.pojo.sp.SpEntity
 
 object SaSpBuilderG {
 
-  def buildSaSp(saSpId: String, sa: SaEntity, sp: SpEntity) = {
+  def buildSaSp(sa: SaEntity, sp: SpEntity, mr: MrEntity) = {
 
-    val sasp = new SaSpEntity(KeysBuilder.getEnvId,saSpId,sa,sp)
+    val sasp = new SaSpEntity(
+      KeysBuilder.getEnvId,
+      KeysBuilder.getSaSpId,
+      sa,
+      sp)
 
     sasp.startDttm = DateBuilder.addMinute(sa.startDt,1)
     sasp.stopDttm  = if (sa.endDt != null) DateBuilder.addMinute(sa.endDt,-1) else sa.endDt
+
+    sasp.startMr = mr
 
     sasp.usageFlg = "Y"
     sasp.usePct = 100
